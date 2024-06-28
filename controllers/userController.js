4f289f849231e5479f4f5689043023e5dc1818ff
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-06-28 16:06:35 +0530

    signin API complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,15 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
+
+//signup controller: 
 const signup = async (req, res) => {
   const { username, password, dateOfBirth } = req.body;
   try {
 
-    const presentUser = await User.findOne({username});
-    if(presentUser){
+    const presentUser = await User.findOne({ username });
+    if (presentUser) {
       return res.status(400).json({
         message: 'user already present'
       });
@@ -22,14 +25,53 @@ const signup = async (req, res) => {
     await user.save();
     res.status(201).json({
       message: 'user signup successful',
-      data:user, 
+      data: user,
     })
   } catch (error) {
     res.status(400).send('Error in Signup of the user: ' + error.message);
   }
 }
 
+
+//signin controller: 
+
+const signin = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({
+        message: 'User not found with this username'
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Invalid Password"
+      });
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+    
+
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+}
 module.exports = {
-  signup
+  signup,
+  signin
 }
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
+
 
 module.exports = router;
